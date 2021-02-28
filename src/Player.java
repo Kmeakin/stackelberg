fbbd48023b27c6128c6f54effc622fe36bd6fcc4
Karl Meakin <karlwfmeakin@gmail.com>
2021-02-28 21:12:46 +0000

    fmt

--- src/Player.java
+++ src/Player.java
@@ -5,23 +5,17 @@ import java.rmi.RemoteException;
 
 /**
  * The RMI interface of the player
+ * 
  * @author Xin
  */
-public interface Player
-	extends Remote
-{
-	public void checkConnection()
-		throws RemoteException;
+public interface Player extends Remote {
+	public void checkConnection() throws RemoteException;
 
-	public void goodbye()
-		throws RemoteException;
+	public void goodbye() throws RemoteException;
 
-	public void startSimulation(final int p_steps)
-		throws RemoteException;
+	public void startSimulation(final int p_steps) throws RemoteException;
 
-	public void endSimulation()
-		throws RemoteException;
+	public void endSimulation() throws RemoteException;
 
-	public void proceedNewDay(final int p_date)
-		throws RemoteException;
+	public void proceedNewDay(final int p_date) throws RemoteException;
 }
